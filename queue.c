8e6ae297f1e0371cd0890f91c16abec2065b23c1
Oscar <seizetheday987456321@gmail.com>
2021-03-26 13:00:33 +0800

    Implement q_reverse function and fix bugs
    Bugs: enhance robustness of q_size

--- queue.c
+++ queue.c
@@ -153,6 +153,8 @@ int q_size(queue_t *q)
     /* TODO: You need to write the code for this function */
     /* Remember: It should operate in O(1) time */
     /* TODO: Remove the above comment when you are about to implement. */
+    if (!q || q->size == 0)
+        return 0;
     return q->size;
 }
 
@@ -165,8 +167,29 @@ int q_size(queue_t *q)
  */
 void q_reverse(queue_t *q)
 {
-    /* TODO: You need to write the code for this function */
-    /* TODO: Remove the above comment when you are about to implement. */
+    if (!q || q->size <= 1)
+        return;
+
+    list_ele_t *pre, *cur, *nex;
+    pre = NULL;
+    cur = q->head;
+    nex = q->head->next;
+
+    q->tail = q->head;
+
+    while (cur->next) {
+        cur->next = pre;
+
+        pre = cur;
+        cur = nex;
+        nex = nex->next;
+    }
+    cur->next = pre;
+
+    pre = cur;
+    // cur = nex;
+
+    q->head = pre;
 }
 
 /*
